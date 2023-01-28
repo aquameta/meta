6bc7ca5fddc269b3004cbb501c7758ba60bdae74
Eric Hanson <eric@aquameta.com>
2023-01-28 20:06:51 +0000

    generator improvements, new component_statement() function
    better approach.  use component_statement() with exec() in a SQL query that queries pg_entity and pg_component
    ```
    select e.name as entity_name, c.name as component_name, exec(component_statement(e.name, c.name))
    from pg_entity e, pg_entity_component c
    order by e.name, c.position;
    ```

--- generator/002-runner.sql
+++ generator/002-runner.sql
@@ -6,41 +6,26 @@ CREATE FUNCTION exec(text) RETURNS text AS $$
 $$ LANGUAGE plpgsql;
 
 /*
-for the given entity, iterate through each component and run it's stmt
-generator function, aggregating the returned strings.
+ * better approach.  use component_statement() with exec() in a SQL query that queries pg_entity and pg_component
+ *
+ * select e.name as entity_name, c.name as component_name, exec(component_statement(e.name, c.name))
+ * from pg_entity e, pg_entity_component c
+ * order by e.name, c.position;
+ */
 
-usage:
-select generate_component_stmts(name) from pg_entity;
-*/
-
-create or replace function generate_component_stmts(entity_name text) returns text /* table (
-    component_id text,
-    component_name uuid references entity_component(id),
-    statement text
-) */ as $$
-declare 
-    components text[];
-    component text;
-    stmt text;
-    statements text := '';
+create or replace function component_statement(entity text, component text) returns text as $$
+declare
+stmt text;
 begin
-    select array_agg(c.name) from (select name from meta_meta.pg_entity_component order by position) c into components;
-    raise info 'components: %', components;
-
-    foreach component in array components loop
-        raise info '__component: %', component;
-        execute format(
-            'select %I(name, constructor_arg_names, constructor_arg_types) from meta_meta.pg_entity where name = %L',
-            'stmt_create_' || component,
-            entity_name
-        ) into stmt;
-        statements := statements || stmt || E'\n';
-    end loop;
-
-    return statements;
-
+    execute format('
+    select %I(name, constructor_arg_names, constructor_arg_types)
+    from meta_meta.pg_entity e
+    where name=%L',
+        'stmt_create_' || component,
+        entity
+    ) into stmt;
+    return stmt;
 end
 $$ language plpgsql;
-
-
+    
 commit;
