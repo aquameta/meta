efe16360dda8ab9fc5fe005bd5e9b721ee29d7ab
Eric Hanson <eric@aquameta.com>
2023-07-02 21:50:06 +0000

    more generated identifiers

--- flat/002-identifiers-nongen.sql
+++ flat/002-identifiers-nongen.sql
@@ -2,6 +2,11 @@
 
 set search_path=meta;
 
+-- for some reason, generator isn't generating these.  they're commented out.
+create function meta.field_id_to_row_id(field_id meta.field_id) returns meta.row_id as $_$select meta.row_id((field_id).schema_name, (field_id).relation_name, (field_id).pk_column_name, (field_id).pk_value) $_$ immutable language sql;
+create cast (meta.field_id as meta.row_id) with function meta.field_id_to_row_id(meta.field_id) as assignment;
+
+
 /*
  * Function: urldecode_arr
  * Author: Marc Mamin
