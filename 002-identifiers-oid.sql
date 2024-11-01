9c13530375aaf66bbefa803a24407215b39652cf
Eric Hanson <eric@aquameta.com>
2024-11-01 02:34:28 -0500

    horse > cart on function_id::oid

--- 002-identifiers-oid.sql
+++ 002-identifiers-oid.sql
@@ -1,6 +1,5 @@
 -- One down, many to go
 
-create cast (meta.function_id as oid) with function meta.function_id_to_oid(meta.function_id) as assignment; 
 create or replace function meta.function_id_to_oid(f meta.function_id) returns oid as $_$
     select p.oid
     from pg_proc p
@@ -11,3 +10,4 @@ create or replace function meta.function_id_to_oid(f meta.function_id) returns o
             select array_agg(t::regtype::oid) from unnest(f.parameters) as t
         )
 $_$ immutable language sql; 
+create cast (meta.function_id as oid) with function meta.function_id_to_oid(meta.function_id) as assignment; 
